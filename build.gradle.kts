c7a38870fe8bb5a041669430232cce799f301e4b
Vadim Tkachenko <vt@seriousinsights.com>
2020-12-05 23:59:12 -0700

    Bumped up SnakeYAML version for a good measure

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 dependencies {
     implementation("org.apache.logging.log4j:log4j-api:2.13.0")
     implementation("org.apache.logging.log4j:log4j-core:2.13.0")
-    implementation("org.yaml:snakeyaml:1.26")
+    implementation("org.yaml:snakeyaml:1.27")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
 }
